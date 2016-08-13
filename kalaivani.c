8a9f45e7a9a20d2ee554349cc00837c5d871b7bb
KalaivaniBalasubramaniyam <kalaivanibala333@gmail.com>
2016-08-13 12:53:19 +0530

    Update kalaivani.c

--- kalaivani.c
+++ kalaivani.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
 int main()
 {
-printf("Hiii");
+  int n;
+  scanf("%d",&n);
+  if(n>0)
+printf("Positive");
+else if(n<0)
+printf("Negative");
+else
+printf("Zero");
 return 0;
 }
